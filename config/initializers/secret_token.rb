5a8a2a825a66b5d10826a74696fdb4ec6885e43b
Ubuntu <ubuntu@ip-10-167-15-194.ec2.internal>
2013-07-30 22:02:14 +0000

    Add files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Adminchurch::Application.config.secret_key_base = '0a1c0f4486c6c82f944b981ac395db288f9614f5e510cec83251f26c93788743867b178720bc5550b97d9bb2a7d45e0e416c1833c1b2d02a33e06e2f1d545b53'
