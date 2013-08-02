86a29c45279377e4244330b49de92c48f64048fa
Marks <artmar89@gmail.com>
2013-08-02 15:31:17 -0500

    plantilla fin

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -2,6 +2,6 @@ class LoginController < ApplicationController
   before_filter :authenticate_user!
   def index
   	answer = user_signed_in? ? current_user.email.to_s : 'Usuario Anonimo'
-  	render :text => answer
+  	redirect_to :controller=>'home', :action=>'index'
   end
 end
